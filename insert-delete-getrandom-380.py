8289ee044554f6b1e5fdd12934f209a12397a323
Aqsa Ashfaq <aqsarao077@gmail.com>
2023-10-25 22:34:51 +0500

    commit

--- insert-delete-getrandom-380.py
+++ insert-delete-getrandom-380.py
@@ -27,11 +27,3 @@ class RandomizedSet:
 
     def getRandom(self) -> int:
         return random.choice(self.elements)
-        
-
-
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
