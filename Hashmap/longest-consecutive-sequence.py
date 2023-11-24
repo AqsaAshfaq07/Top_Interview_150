9eb63b3b91badb4477248c4fd81609d310aacbec
Aqsa Ashfaq <aqsarao077@gmail.com>
2023-11-24 11:36:50 +0500

    commit

---
+++ Hashmap/longest-consecutive-sequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestConsecutive(self, nums):
+        nums = set(nums)
+        res = 0
+        for num in nums:
+            if num - 1 not in nums:
+                nextNum = num + 1
+                while nextNum in nums:
+                    nextNum += 1
+                res = max(res, nextNum - num)
+        return res
